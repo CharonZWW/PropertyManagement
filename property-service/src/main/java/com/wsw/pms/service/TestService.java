0290e945fd848d86f97c84c6c07567d726f2c3f5
Charon <517399181@qq.com>
2018-07-20 14:58:11 +0800

    First environment configuration

---
+++ property-service/src/main/java/com/wsw/pms/service/TestService.java
@@ -0,0 +1,13 @@
+package com.wsw.pms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("TestService")
+public class TestService {
+        
+	public void test(){
+		
+		System.out.println("service成功");
+	}
+	
+}
